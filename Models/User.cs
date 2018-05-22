2bb0f3840074ce0de4c6ce4b495a3bf4ed89612f
Daniel Clancy <danielclancy@Daniels-MBP.hsd1.il.comcast.net>
2018-05-21 20:40:28 -0500

    finished homepage and built user model

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace UserDashboard {
     
     public class User {
-        
+        [Key]
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public int UserLevel { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 }
