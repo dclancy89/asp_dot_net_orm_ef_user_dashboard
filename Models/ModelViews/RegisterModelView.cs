003494e8825ec6004cdccf7ace31e308220b7f18
Daniel Clancy <danielclancy@Daniels-MBP.hsd1.il.comcast.net>
2018-05-21 20:46:00 -0500

    added register view model

---
+++ Models/ModelViews/RegisterModelView.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UserDashboard
+{
+    public class RegisterViewModel {
+        [Required]
+        [EmailAddress]
+        [Display(Name="Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [RegularExpression(@"^[a-zA-Z]+$")]
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [RegularExpression(@"^[a-zA-Z]+$")]
+        [Display(Name="Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [Display(Name="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage="Passwords must match.")]
+        [DataType(DataType.Password)]
+        [Display(Name="Conform Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
